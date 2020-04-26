1ce4829a612a93bec30fa5fa1cf81154710ec029
Ryan <obviouslyghosts@gmail.com>
2020-04-26 00:34:36 -0400

    tweaking movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-  public float moveSpeed = 5f;
+  public float moveSpeed = 4f;
   public Transform movePoint;
 
   public LayerMask whatStopsMovement;
