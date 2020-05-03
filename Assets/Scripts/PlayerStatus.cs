eedadadb5e36949b22eca23cb39b229bf2098c29
Ryan <obviouslyghosts@gmail.com>
2020-05-02 23:33:31 -0400

    tied to values

--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStatus : MonoBehaviour
 {
+  public Text UIhealth;
+  public Text UIwealth;
 
   public int health;
   public int wealth;
@@ -11,11 +14,13 @@ public class PlayerStatus : MonoBehaviour
   public void Attacked(int damage)
   {
     health -= damage;
+    UIhealth.text = health.ToString();
   }
 
   public void Spew(int value)
   {
     wealth -= value;
+    UIwealth.text = wealth.ToString();
   }
 
 }
