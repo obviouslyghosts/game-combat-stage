4f7b6afb0e56b082a5b56e4bfd5143aa7e5a1cd1
Ryan <obviouslyghosts@gmail.com>
2020-05-03 23:27:44 -0400

    notes update

--- Assets/Scripts/NOTES.cs
+++ Assets/Scripts/NOTES.cs
@@ -6,16 +6,17 @@ public class NOTES : MonoBehaviour
 {
 
   // NEXT NEXT NEXT NEXT
-  // screens
   // level up progress
-  // title menu
+  // title menu art
   // More enemies
   // more treasure
+  // UI SFX
 
 
   // FIX FIX FIX FIX
   // fix player 'infinite slide'
   // enemy collision (self, and moving)
   // enemy attack timer!
+  // player colission on bottom of screen?
 
 }
