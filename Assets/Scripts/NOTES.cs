c5c45d740985ba92f2bbf8c0d5c73656be9a5a91
Ryan <obviouslyghosts@gmail.com>
2020-05-26 23:39:51 -0400

    notes update

--- Assets/Scripts/NOTES.cs
+++ Assets/Scripts/NOTES.cs
@@ -6,32 +6,23 @@ public class NOTES : MonoBehaviour
 {
 
   // NEXT NEXT NEXT NEXT
+  // enemy tally
   // level up progress
   // title menu art
   // spew physics!
   // More enemies
   // more treasure
   // UI SFX
+  // Story
 
 
   // FIX FIX FIX FIX
-  // fix player 'infinite slide'
   // enemy collision (self, and moving)
   // enemy attack timer!
 
   // GAME LOOP
   // pickup your own!
-  //
-  // enemy comes
-  // spew equip
-  // enemy hits weight limit
-  // enemy drops +1 item (tag as loot 2)
-  // collect shit before enemys grab it!
-  // enemies go away
-  //
-  // it's magic - they just manifest
-  // wheres the challenge?
-  // mana bar? slowly recharches, fixed spend
+
 
 
 }
